6591e81a7faff08065edb386f2532ae655cb2696
Matt Scaccia <scaccimj202@potsdam.edu>
2020-03-29 13:45:09 -0400

    Restructured directory, created a test method 'fact' to test StackADT, Node, and LStack

--- FactorialStack.java
+++ FactorialStack.java
@@ -6,6 +6,22 @@
  *description: Factorial as a stack adapted from Clifford Shaffers Data Structures and Algorithim Analysis, Chapter 4.2.4
  */
 
- public class FactorialStack{
-     
+public class FactorialStack{
+
+    public static void main(String[] args){
+        System.out.println(fact(3));
+        System.out.println(fact(4));
+        System.out.println(fact(5));
+    }
+
+    public static long fact(int n){
+        assert (n >= 0 ) && (n <= 20) : "Number is out of range";
+        LStack<Integer> stack = new LStack<Integer>(n);
+        while(n > 1)
+            stack.push(n--);
+        long result = 1;
+        while (stack.length() > 0)
+            result = result * stack.pop();
+        return result;
+    }
  }
