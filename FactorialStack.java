000f0232d76ffe8fe1f073dc15b064f5676a4629
Matt Scaccia <scaccimj202@potsdam.edu>
2020-03-29 13:36:03 -0400

    Initial commit

---
+++ FactorialStack.java
@@ -0,0 +1,11 @@
+/**
+ *@author Matthew Scaccia
+ *course CIS303
+ *@since 3/29/2020
+ *@assignment 4b
+ *description: Factorial as a stack adapted from Clifford Shaffers Data Structures and Algorithim Analysis, Chapter 4.2.4
+ */
+
+ public class FactorialStack{
+     
+ }
